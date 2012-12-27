6f6ccc0b47b3b1ae3a5b2b7067ade75497a009be
Brian Friesen <friesen@ou.edu>
2012-12-27 17:25:10 -0600

    Removed the rest of the GSL vector/matrix statements in place of native C arrays.

--- src/ode_rhs.c
+++ src/ode_rhs.c
@@ -14,97 +14,22 @@
  *           this case) */
 
 int ode_rhs(double t, const double y[], double dydt[], void *params) {
-  // convert C arrays to GSL vectors
-  gsl_vector_view dydt_vec = gsl_vector_view_array(dydt, nvar);
-  gsl_vector *ode_rhs = &dydt_vec.vector;
-
-  // same here
-  gsl_vector_const_view y_vec = gsl_vector_const_view_array(y, nvar);
-  const gsl_vector *Y_i = &y_vec.vector;
 
   // get the temperature
   double T = *(double *)params;
 
-  // the RHS of each ODE
-  gsl_vector_set(ode_rhs, 0, lambda_ijT_avg(6, 12, T, 'a') * gsl_vector_get(Y_i, 6)
-                 * gsl_vector_get(Y_i, 12)
-		 + lambda_ijT_avg(9, 12, T, 'a') * gsl_vector_get(Y_i, 9)
-		 * gsl_vector_get(Y_i, 12)
-		 + lambda_ijT(11, 12, T) * gsl_vector_get(Y_i, 11)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 1, -lambda_ijT(1, 12, T) * gsl_vector_get(Y_i, 1)
-                 * gsl_vector_get(Y_i, 12)
-		 + lambda_ijT_avg(6, 12, T, 'a') * gsl_vector_get(Y_i, 6)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 2, lambda_ijT(1, 12, T) * gsl_vector_get(Y_i, 1)
-                 * gsl_vector_get(Y_i, 12)
-		 - lambda_ij_beta(2) * gsl_vector_get(Y_i, 2));
-
-  gsl_vector_set(ode_rhs, 3, lambda_ij_beta(2) * gsl_vector_get(Y_i, 2)
-                 - lambda_ijT(3, 12, T) * gsl_vector_get(Y_i, 3)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 4, -lambda_ijT(4, 12, T) * gsl_vector_get(Y_i, 4)
-                 * gsl_vector_get(Y_i, 12)
-		 + lambda_ijT(3, 12, T) * gsl_vector_get(Y_i, 3)
-		 * gsl_vector_get(Y_i, 12)
-		 + lambda_ijT_avg(9, 12, T, 'a') * gsl_vector_get(Y_i, 9)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 5, -lambda_ij_beta(5) * gsl_vector_get(Y_i, 5)
-                 + lambda_ijT(4, 12, T) * gsl_vector_get(Y_i, 4)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 6, lambda_ij_beta(5) * gsl_vector_get(Y_i, 5)
-                 - lambda_ijT_avg(6, 12, T, 'a') * gsl_vector_get(Y_i, 6)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT_avg(6, 12, T, 'g') * gsl_vector_get(Y_i, 6)
-		 * gsl_vector_get(Y_i, 12)
-		 + lambda_ijT(11, 12, T) * gsl_vector_get(Y_i, 11)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 7, lambda_ijT_avg(6, 12, T, 'g') * gsl_vector_get(Y_i, 6)
-                 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT(7, 12, T) * gsl_vector_get(Y_i, 7)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 8, lambda_ijT(7, 12, T) * gsl_vector_get(Y_i, 7)
-                 * gsl_vector_get(Y_i, 12)
-		 - lambda_ij_beta(8) * gsl_vector_get(Y_i, 8));
-
-  gsl_vector_set(ode_rhs, 9, lambda_ij_beta(8) * gsl_vector_get(Y_i, 8)
-                 - lambda_ijT_avg(9, 12, T, 'g') * gsl_vector_get(Y_i, 9)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT_avg(9, 12, T, 'a') * gsl_vector_get(Y_i, 9)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 10, lambda_ijT_avg(9, 12, T, 'g') * gsl_vector_get(Y_i, 9)
-                 * gsl_vector_get(Y_i, 12)
-		 - lambda_ij_beta(10) * gsl_vector_get(Y_i, 10));
-
-  gsl_vector_set(ode_rhs, 11, lambda_ij_beta(10) * gsl_vector_get(Y_i, 10)
-                 - lambda_ijT(11, 12, T) * gsl_vector_get(Y_i, 11)
-		 * gsl_vector_get(Y_i, 12));
-
-  gsl_vector_set(ode_rhs, 12, -lambda_ijT(1, 12, T) * gsl_vector_get(Y_i, 1)
-                 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT(3, 12, T) * gsl_vector_get(Y_i, 3)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT(4, 12, T) * gsl_vector_get(Y_i, 4)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT_avg(6, 12, T, 'a') * gsl_vector_get(Y_i, 6)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT_avg(6, 12, T, 'g') * gsl_vector_get(Y_i, 6)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT(7, 12, T) * gsl_vector_get(Y_i, 7)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT_avg(9, 12, T, 'a') * gsl_vector_get(Y_i, 9)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT_avg(9, 12, T, 'g') * gsl_vector_get(Y_i, 9)
-		 * gsl_vector_get(Y_i, 12)
-		 - lambda_ijT(11, 12, T) * gsl_vector_get(Y_i, 11)
-		 * gsl_vector_get(Y_i, 12));
+  dydt[ 0] =  lambda_ijT_avg(6, 12, T, 'a') * y[6] * y[12] + lambda_ijT_avg(9, 12, T, 'a') * y[9] * y[12] + lambda_ijT(11, 12, T) * y[11] * y[12];
+  dydt[ 1] = -lambda_ijT(1, 12, T) * y[1] * y[12] + lambda_ijT_avg(6, 12, T, 'a') * y[6] * y[12];
+  dydt[ 2] =  lambda_ijT(1, 12, T) * y[1] * y[12] - lambda_ij_beta(2) * y[2];
+  dydt[ 3] =  lambda_ij_beta(2) * y[2] - lambda_ijT(3, 12, T) * y[3] * y[12];
+  dydt[ 4] = -lambda_ijT(4, 12, T) * y[4] * y[12] + lambda_ijT(3, 12, T) * y[3] * y[12] + lambda_ijT_avg(9, 12, T, 'a') * y[9] * y[12];
+  dydt[ 5] = -lambda_ij_beta(5) * y[5] + lambda_ijT(4, 12, T) * y[4] * y[12];
+  dydt[ 6] =  lambda_ij_beta(5) * y[5] - lambda_ijT_avg(6, 12, T, 'a') * y[6] * y[12] - lambda_ijT_avg(6, 12, T, 'g') * y[6] * y[12] + lambda_ijT(11, 12, T) * y[11] * y[12];
+  dydt[ 7] =  lambda_ijT_avg(6, 12, T, 'g') * y[6] * y[12] - lambda_ijT(7, 12, T) * y[7] * y[12];
+  dydt[ 8] =  lambda_ijT(7, 12, T) * y[7] * y[12] - lambda_ij_beta(8) * y[8];
+  dydt[ 9] =  lambda_ij_beta(8) * y[8] - lambda_ijT_avg(9, 12, T, 'g') * y[9] * y[12] - lambda_ijT_avg(9, 12, T, 'a') * y[9] * y[12];
+  dydt[10] =  lambda_ijT_avg(9, 12, T, 'g') * y[9] * y[12] - lambda_ij_beta(10) * y[10];
+  dydt[11] =  lambda_ij_beta(10) * y[10] - lambda_ijT(11, 12, T) * y[11] * y[12];
+  dydt[12] = -lambda_ijT(1, 12, T) * y[1] * y[12] - lambda_ijT(3, 12, T) * y[3] * y[12] - lambda_ijT(4, 12, T) * y[4] * y[12] - lambda_ijT_avg(6, 12, T, 'a') * y[6] * y[12] - lambda_ijT_avg(6, 12, T, 'g') * y[6] * y[12] - lambda_ijT(7, 12, T) * y[7] * y[12] - lambda_ijT_avg(9, 12, T, 'a') * y[9] * y[12] - lambda_ijT_avg(9, 12, T, 'g') * y[9] * y[12] - lambda_ijT(11, 12, T) * y[11] * y[12];
   return GSL_SUCCESS;
 }
